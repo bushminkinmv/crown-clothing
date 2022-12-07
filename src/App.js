664da66609ffedb557123e44a3603e1ef8842115
Max Bushminkin <bushminkinmv@gmail.com>
2022-12-07 19:32:03 +0100

    added sass to project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './categories.styles.scss';
+
 const categories = [
   {
     id: 1,
@@ -24,10 +26,10 @@ const categories = [
 const App = () => {
   return (
     <div className="categories-container">
-    {categories.map(({ title }) => (
-      <div className="category-container">
+    {categories.map(({ title, id }) => (
+      <div key={id} className="category-container">
         <div className="background-image" />
-        <div className="category-body">
+        <div className="category-body-container">
           <h2>{ title }</h2>
           <p>Show now</p>
         </div>
