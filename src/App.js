ba49ebd41410ce3cf19436ab9a438a6fa5e1e715
Max Bushminkin <bushminkinmv@gmail.com>
2022-12-07 18:19:56 +0100

    setted up categories

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ const categories = [
   {
     id: 1,
     title: 'hats',
-  }
+  },
+  {
+    id: 2,
+    title: 'jackets',
+  },
+  {
+    id: 3,
+    title: 'sneakers',
+  },
+  {
+    id: 4,
+    title: 'womens',
+  },
+  {
+    id: 5,
+    title: 'mans',
+  },
 ];
 
 const App = () => {
@@ -10,7 +26,7 @@ const App = () => {
     <div className="categories-container">
     {categories.map(({ title }) => (
       <div className="category-container">
-        {/* <img /> */}
+        <div className="background-image" />
         <div className="category-body">
           <h2>{ title }</h2>
           <p>Show now</p>
