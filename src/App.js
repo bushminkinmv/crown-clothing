11a63dab5a25b6687e6a815772a0517f829dfb25
Max Bushminkin <bushminkinmv@gmail.com>
2022-12-07 17:18:07 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+const categories = [
+  {
+    id: 1,
+    title: 'hats',
+  }
+];
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+    {categories.map(({ title }) => (
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body">
+          <h2>{ title }</h2>
+          <p>Show now</p>
+        </div>
+      </div>
+    ))}
     </div>
-  );
-}
+    )
+};
 
 export default App;
