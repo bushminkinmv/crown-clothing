b0d17acd52b91acf298dcf7a6eb41cfb9cd8c9be
Max Bushminkin <bushminkinmv@gmail.com>
2023-01-12 15:41:05 +0100

    added cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,8 @@ import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase.utils';
 import './navigation.styles.scss';
@@ -28,7 +30,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
