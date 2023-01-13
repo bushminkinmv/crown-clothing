cced13cd03791dedf87ed17e4e0be52b453b4876
Max Bushminkin <bushminkinmv@gmail.com>
2023-01-13 20:23:19 +0100

    finished checkout component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Authentication from './routes/authentication/authentication';
+import Checkout from './routes/checkout/checkout.component';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Shop from './routes/shop/shop.component';
@@ -12,6 +13,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='auth' element={<Authentication />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
